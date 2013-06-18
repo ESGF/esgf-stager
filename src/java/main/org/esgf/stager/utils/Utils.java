5becdf87e8e9e12f34ed5fbbdd4b7c33540921bc
Harney <8xo@lap00091497.ornl.gov>
2013-06-18 17:27:34 -0400

    Removed "etc" and "build" requirements on build.xml (temporarily so that it may be built)

--- src/java/main/org/esgf/stager/utils/Utils.java
+++ src/java/main/org/esgf/stager/utils/Utils.java
@@ -4,4 +4,10 @@ public class Utils {
 
 	public static String responseMessage = "Stager successfully launched";
 	
+	
+	public static String default_node = "esg2-sdnl1.ccs.ornl.gov";
+	//public static String defaulf_"";
+	public static String default_srm_url = 
+			"url=srm://esg2-sdnl1.ccs.ornl.gov:46790/srm/v2/server?SFN=mss://esg2-sdnl1.ccs.ornl.gov//proj/cli049/UHRGCS/ORNL/CESM1/t341f02.FAMIPr/atm/hist/t341f02.FAMIPr.cam2.h0.1978-10.nc";
+	
 }
